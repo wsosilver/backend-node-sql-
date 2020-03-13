443d2fcf827f8d522f170e9b2fea2a5ba380c2e3
WENDERSON SILVEIRA <WSO.SILVER@GMAIL.COM>
2020-03-13 09:46:57 -0300

    Api funcionando

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();         
 const bodyParser = require('body-parser');
 const port = 3000; //porta padrão
@@ -13,6 +15,12 @@ sql.connect(connStr)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use((req,res,next) =>{
+    res.header("Access-Control-Allow-Origin", "*")
+    app.use(cors());
+    next();
+    //console.log("acessoou o Midle")
+});
 
 
 //definindo as rotas
@@ -31,8 +39,13 @@ function execSQLQuery(sqlQry, res){
 }
 
 let str = "SET dateformat ymd; SELECT tu.cod_loja, lj.des_loja, tu.dat_movime, Sum(val_venbal) AS Val_VenBal, Sum(qtd_venbal) AS Qtd_VenBal, Sum(val_vendom) AS Val_VenDom, Sum(qtd_vendom) AS Qtd_VenDom, Sum(val_devolu) AS Val_Devolu, Sum(val_troca)  AS Val_Troca, Sum(val_descon) AS Val_Descon, Sum(val_dinfat) AS Val_DinFat, Sum(val_chvfat) AS Val_ChvFat, Sum(val_chpfat) AS Val_ChpFat, Sum(val_cmgfat) AS Val_CmgFat, Sum(val_cvnfat) AS Val_CvnFat, Sum(val_przfat) AS Val_PrzFat, Sum(val_pbmfat) AS Val_PbmFat, Sum(val_fidfat) AS Val_FidFat, Sum(val_dinest + val_dindev) AS Val_DinDev, Sum(val_chvest + val_chvdev) AS Val_ChvDev, Sum(val_chpest + val_chpdev) AS Val_ChpDev, Sum(val_cmgest + val_cmgdev) AS Val_CmgDev, Sum(val_cvnest + val_cvndev) AS Val_CvnDev, Sum(val_przest + val_przdev) AS Val_PrzDev, Sum(val_pbmest + val_pbmdev) AS Val_PbmDev, Sum(val_fidest + val_fiddev) AS Val_FidDev FROM turno tu INNER JOIN lojas lj ON tu.cod_loja = lj.cod_loja WHERE tu.dat_movime BETWEEN '2020-03-10 00:00:00' AND '2020-03-10 00:00:00' AND tu.cod_loja IN ( 1 ) GROUP BY tu.cod_loja,lj.des_loja,tu.dat_movime ORDER BY tu.cod_loja,tu.dat_movime"
-router.get('/dashboard', (req, res) =>{
+router.get('/dashboard', cors() ,(req, res) =>{
     execSQLQuery(str, res);
+})
+
+
+router.get('/empre', cors() ,(req, res) =>{
+    execSQLQuery("select cod_loja,des_loja from Lojas where Flg_Inativ = 0 ", res);
 })
 
 router.get('/produ')
